a6a59181b4e111c16255eabc6a2c0da6afc40d84
RunNoobs1 <157906582+RunNoobs1@users.noreply.github.com>
2024-01-27 21:04:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["The best LOOT.GG predictor, and the only one that really works."];
 
 
 let currentIndex = 0;
